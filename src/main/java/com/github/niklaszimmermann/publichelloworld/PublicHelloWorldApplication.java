75e5073ce8537e02079956c8e113ce1ff921cb6c
niklaszimmermann <mail@niklaszimmermann.de>
2022-04-28 14:56:25 +0200

    init hello world

---
+++ src/main/java/com/github/niklaszimmermann/publichelloworld/PublicHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.github.niklaszimmermann.publichelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicHelloWorldApplication.class, args);
+	}
+
+}
